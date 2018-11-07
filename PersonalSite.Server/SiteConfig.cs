6255a57b636f2ac3fad12027962b9174ba501ecd
Nick Galvin <ngalvin91@yahoo.com>
2018-11-06 20:52:51 -0600

    S3 Upload and SiteConfig

---
+++ PersonalSite.Server/SiteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSite.Server
+{
+    public class SiteConfig
+    {
+        public AWSConfig AWS { get; set; }
+    }
+
+    public class AWSConfig
+    {
+        public string S3_Bucket { get; set; }
+    }
+}
---
+++ Site.Core/Upload/FIleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Site.Core.Upload
+{
+    public class FileService
+    {
+
+    }
+}
