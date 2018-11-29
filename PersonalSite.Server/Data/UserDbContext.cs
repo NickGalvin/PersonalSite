5caf208a037fd6a518f434bea53c6459d0143541
Nick Galvin <ngalvin91@yahoo.com>
2018-11-28 21:07:24 -0600

    login started and file stuff

---
+++ PersonalSite.Server/Data/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using PersonalSite.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSite.Server.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Site.Core/Metadata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Site.Core
+{
+    public class Metadata
+    {
+        public string ItemId { get; set; }
+
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+        public string Source { get; set; }
+        public string Format { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
