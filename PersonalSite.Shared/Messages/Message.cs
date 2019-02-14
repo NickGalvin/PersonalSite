b335f7fc5c5c55774bd1533f0a62047228a46b73
nick <nick@furnituredealer.net>
2019-02-13 20:37:01 -0600

    .

---
+++ PersonalSite.Shared/Messages/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalSite.Shared.Messages
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
